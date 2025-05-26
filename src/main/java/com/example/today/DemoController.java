c5de8b495fcecd2915961da9f62a0f69350a4706
Sorin Gore <soringoredev@gmail.com>
2025-05-26 18:17:23 +0300

    Modify *

--- src/main/java/com/example/today/DemoController.java
+++ src/main/java/com/example/today/DemoController.java
@@ -1,11 +1,17 @@
 package com.example.today;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class DemoController {
+
+    @GetMapping("/dailyrecipe")
+    public String dailyRecipe() {
+        return myChef.getDailyRecipe();
+    }
 
     private Chef myChef;
 
@@ -15,5 +21,4 @@ public class DemoController {
     public DemoController(Chef theChef) {
         myChef = theChef;
     }
-
 }
