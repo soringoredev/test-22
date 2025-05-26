d32cd6dc8319149fba7eb55c6e4259395e0d2c63
Sorin Gore <soringoredev@gmail.com>
2025-05-26 20:34:37 +0300

    chore: Set @Primary

--- src/main/java/com/example/today/common/TurkishChef.java
+++ src/main/java/com/example/today/common/TurkishChef.java
@@ -1,8 +1,10 @@
 package com.example.today.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class TurkishChef implements Chef {
 
     @Override
